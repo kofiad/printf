399bd5d3f3ae73e1a704cc235ec9f51ac0a6d4da
kofiad <kofiadaba@gmail.com>
2023-11-13 14:33:56 +0000

    I have created the _printf.c file, yet to put in actual code

---
+++ _printf.c
@@ -0,0 +1,10 @@
+include "main.h"
+
+/**
+ * _printf - a function that produces output according to a format
+ * @format: is a character string
+ * Return: the number of characters printed
+ * (excluding the null byte used to end output to strings)
+ */
+int _printf(const char *format, ...)
+
