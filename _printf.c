2f553e8b34f9d6e2b1c6db782b970208a173a740
kofiad <kofiadaba@gmail.com>
2023-11-14 22:40:29 +0000

    I have updated the printf file and the header file

--- _printf.c
+++ _printf.c
@@ -7,4 +7,40 @@ include "main.h"
  * (excluding the null byte used to end output to strings)
  */
 int _printf(const char *format, ...)
+{
+	va_list args;
+	int char_count = 0;
 
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%' && *(format + 1) != '\0')
+		{
+			format++;/*move past '%'*/
+			switch (*format)
+			{
+				case 'c':
+					char_count += write(1, &va_arg(args, int), 1);
+				break;
+				case 's':
+				char_count += write(1, va_arg(args, char*), write(va_arg(args, char*), 1));
+				break;
+				case '%';
+				char_count += write(1, "%", 1);
+				break;
+				default:
+				write(1, format, 1);
+				char_count++;
+			}
+		}
+		else
+		{
+			write(1, format, 1);
+			char_count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (char_count);
+}
--- main.h
+++ main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*including standard C libraries*/
 #include <unistd.h>
 #include <stdio.h>
 #include <stdarg.h>
@@ -43,7 +42,69 @@ int _printf(const char *format, ...);
 int process_format(const char *specifier, int *index, va_list arguments,
 		char buffer[], int flags, int width, int precision, int size);
 
-/*function implementatio*/
+/*function implementation*/
+/*character and string printing functions*/
+int print_character(va_list args, char buffer[], int flags, int width,
+		int precision, int size);
+int print_string(va_list args, char buffer[], int flags, int width,
+		int precision, int size);
+int print_percent(va_list args, char buffer[], int flags, int width,
+		int precision, int size);
 
+/*number printing fuctions*/
+int print_integer(va_list args, char buffer[], int flags, int width,
+		int precision, int size);
+int print_binary(va_list args, char buffer[], int flags, int width,
+		int precision, int size);
+int print_unsigned(va_list args, char buffer[], int flags, int width,
+		int precision, int size);
+int print_octal(va_list args, char buffer[], int flags, int width,
+		int precision, int size);
+int print_hexadecimal(va_list args, char buffer[], int flags, int width,
+		int precision, int size);
+int print_hex_upper(va_list args, char buffer[], int flags, int width,
+		int precision, int size);
+
+/*non-printable character handling*/
+int print_non_printable(va_list args, char buffer[], int flags, int width,
+		int precision, int size);
+
+/*memory address printing*/
+int print_memory_address(va_list args, char buffer[], int flags,
+		int width, int precision, int size);
+
+/*other specifier handling functions*/
+int extract_flags(const char *format, int *index);
+int extract_width(const char *format, int *index, va_list arguments);
+int extract_precision(const char *format, int *index, va_list arguments);
+int extract_size(const char *format, int *index);
+
+/*reverse string printing function*/
+int print_reversed(va_list args, char buffer[], int flags, int width,
+		int precision, int size);
+
+/*rot13 string printing function*/
+int print_rot13_string(va_list args, char buffer[], int flags, int width,
+		int precision, int size);
+
+/*width handler functions*/
+int handle_char_write(char c, char buffer[], int flags, int width,
+		int precision, int size);
+int write_number(int is_positive, int index, char buffer[], int flags,
+		int width, int precision, int size);
+int write_num(int index, char buffer[], int flags, int width,
+		int precision, int length, char padding, char extra_char);
+int write_memory_address(int index, char buffer[], int flags, int width,
+		int length, char padding, int padding_start,
+		char extra_char);
+int write_unsigned(int is_negative, int index, char buffer[], int flags,
+		int width, int precision, int size);
+
+/*utility functions*/
+int is_printable_char(char character);
+int append_hexadec_code(char character, char buffer[], int index);
+int is_digit_char(char character);
+long int convert_size_number(long int number, int size);
+long int convert_size_unsigned(unsigned long int number, int size);
 
 #endif /*MAIN_H*/
