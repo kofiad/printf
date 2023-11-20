2baba26a9d1bd4b39147cbdc39fbfab93940fd79
Bright Sefah <sefahb13@gmail.com>
2023-11-20 10:25:39 +0000

    Bugfix

--- _printf.c
+++ _printf.c
@@ -1,5 +1,7 @@
-include "main.h"
-
+#include "main2.h"
+#include <stdarg.h>
+#include <unistd.h>
+#define BUFFER_SIZE 1024
 /**
  * _printf - a function that produces output according to a format
  * @format: is a character string
@@ -7,4 +9,29 @@ include "main.h"
  * (excluding the null byte used to end output to strings)
  */
 int _printf(const char *format, ...)
+{
+	va_list format_specifiers;
+	int num, len;
+	char *buffer;
 
+	va_start(format_specifiers, format);
+
+	while (*format)
+	{
+
+		switch (*format)
+		{
+			case ('%'):
+				case ('d'):
+					num = va_arg(format_specifiers, int);
+					write(1, num, 1);
+					break;
+			default:
+				write(1, format, 1);
+				break;
+		}
+
+		format++;
+	}
+	va_end(format_specifiers);
+}
